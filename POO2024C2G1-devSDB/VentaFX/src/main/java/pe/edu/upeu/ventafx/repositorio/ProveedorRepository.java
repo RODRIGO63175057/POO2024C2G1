1c1839f53014baba674363726e3c25f7d4101db7
unknown <you@example.com>
2024-11-26 11:27:32 -0500

    auto complete

--- POO2024C2G1-devSDB/VentaFX/src/main/java/pe/edu/upeu/ventafx/repositorio/ProveedorRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.ventafx.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upeu.ventafx.modelo.Proveedor;
-
-@Repository
-public interface ProveedorRepository  extends JpaRepository<Proveedor, Long> {
-}
